05a157641e61c2e430d7873de3b228d705c43702
Prabhu Kiran Konda <prabhukiran8790@pm.me>
2023-10-30 20:38:05 +0530

    removed dotenv

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,9 +1,9 @@
 import type { Config } from 'drizzle-kit';
-import * as dotenv from 'dotenv';
+// import * as dotenv from 'dotenv';
 
-dotenv.config({
-	path: '.env'
-});
+// dotenv.config({
+// 	path: '.env'
+// });
 
 export default {
 	schema: './src/lib/db/schema.ts',
--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,11 +1,11 @@
 import { neon, neonConfig } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './schema';
-import * as dotenv from 'dotenv';
+// import * as dotenv from 'dotenv';
 
-dotenv.config({
-	path: '.env'
-});
+// dotenv.config({
+// 	path: '.env'
+// });
 
 neonConfig.fetchConnectionCache = true;
 
